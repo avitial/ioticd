f69387a240512b61c7edf694aff926c8f28b9de9
leavitia <lavitia9@gmail.com>
2017-02-28 14:51:48 -0800

    Updated programs to add LED indicators (easy to track packets)

--- lora/src/lora_tx/lora_tx.ino
+++ lora/src/lora_tx/lora_tx.ino
@@ -1,4 +1,4 @@
-// LoRa 9x_TX
+// LoRa 915MHz_TX
 // -*- mode: C++ -*-
 // Example sketch showing how to create a simple messaging client (transmitter)
 // with the RH_RF95 class. RH_RF95 class does not provide for addressing or
@@ -13,99 +13,107 @@
 #define RFM95_RST 9
 #define RFM95_INT 2
 
-// Change to 434.0 or other frequency, must match RX's freq!
+// change to 915.0 MHz, it must match RX's frequency!
 #define RF95_FREQ 915.0
-#define LED_RCVD 6
-#define LED_STBY 7
+#define LED_STBY 5 
+#define LED_SENT 6 
+#define LED_LOST 7 
 
-// Singleton instance of the radio driver
+// packet statistics
+int sent_pkt = 0;
+int rcvd_pkt = 0;
+int lost_pkt = 0;
+
+// singleton instance of the radio driver
 RH_RF95 rf95(RFM95_CS, RFM95_INT);
 
-void setup() 
-{
-  pinMode(LED_RCVD, OUTPUT);
-  pinMode(LED_STBY, OUTPUT);
-  pinMode(RFM95_RST, OUTPUT);
-  digitalWrite(RFM95_RST, HIGH);
+void setup(){
+	pinMode(LED_STBY, OUTPUT);
+	pinMode(LED_SENT, OUTPUT);
+	pinMode(LED_LOST, OUTPUT);
+	pinMode(RFM95_RST, OUTPUT);
+	digitalWrite(RFM95_RST, HIGH);
+	digitalWrite(LED_STBY, HIGH);
+	digitalWrite(LED_SENT, LOW);
+	digitalWrite(LED_LOST, LOW);
 
-  while (!Serial);
-  Serial.begin(9600);
-  delay(100);
+	while(!Serial);
+	Serial.begin(9600); delay(100);
 
-  Serial.println("Arduino LoRa TX Test!");
+	Serial.println("Arduino LoRa TX Test!");
 
-  // manual reset
-  digitalWrite(RFM95_RST, LOW);
-  delay(10);
-  digitalWrite(RFM95_RST, HIGH);
-  delay(10);
+	// manual reset
+	digitalWrite(RFM95_RST, LOW); delay(10);
+	digitalWrite(RFM95_RST, HIGH); delay(10);
 
-  while (!rf95.init()) {
-    Serial.println("LoRa radio init failed");
-    while (1);
-  }
-  Serial.println("LoRa radio init OK!");
+	while(!rf95.init()){
+		Serial.println("LoRa radio init failed");
+		while(1);
+	}
+	Serial.println("LoRa radio init OK!");
 
-  // Defaults after init are 434.0MHz, modulation GFSK_Rb250Fd250, +13dbM
-  if (!rf95.setFrequency(RF95_FREQ)) {
-    Serial.println("setFrequency failed");
-    while (1);
-  }
-  Serial.print("Set Freq to: "); Serial.println(RF95_FREQ);
-  
-  // Defaults after init are 434.0MHz, 13dBm, Bw = 125 kHz, Cr = 4/5, Sf = 128chips/symbol, CRC on
+	// Defaults after init are 434.0MHz, modulation GFSK_Rb250Fd250, +13dbM
+	if(!rf95.setFrequency(RF95_FREQ)) {
+	Serial.println("setFrequency failed");
+	while(1);
+	}
+	Serial.print("Set Freq to: "); Serial.println(RF95_FREQ);
 
-  // The default transmitter power is 13dBm, using PA_BOOST.
-  // If you are using RFM95/96/97/98 modules which uses the PA_BOOST transmitter pin, then 
-  // you can set transmitter powers from 5 to 23 dBm:
-  rf95.setTxPower(23, false);
+	// Defaults after init are 434.0MHz, 13dBm, Bw = 125 kHz, Cr = 4/5, Sf = 128chips/symbol, CRC on
+
+	// The default transmitter power is 13dBm, using PA_BOOST.
+	// If you are using RFM95/96/97/98 modules which uses the PA_BOOST transmitter pin, then 
+	// you can set transmitter powers from 5 to 23 dBm:
+	rf95.setTxPower(23, false);
 }
 
 int16_t packetnum = 0;  // packet counter, we increment per xmission
 
-void loop()
-{
-  Serial.println("Sending to rf95_server");
-  // Send a message to rf95_server
-  
-  char radiopacket[20] = "Hello World #      ";
-  itoa(packetnum++, radiopacket+13, 10);
-  Serial.print("Sending "); Serial.println(radiopacket);
-  radiopacket[19] = 0;
-  
-  Serial.println("Sending..."); delay(10);
-  rf95.send((uint8_t *)radiopacket, 20);
+void loop(){
+	digitalWrite(LED_STBY, HIGH);
+	Serial.println("Sending to rf95_server");
+	// Send a message to rf95_server
+	char radiopacket[20] = "Hello World #      ";
+	itoa(packetnum++, radiopacket+13, 10);
+	Serial.print("Sending "); Serial.println(radiopacket);
+	radiopacket[19] = 0;
+	Serial.println("Sending..."); delay(10);
+	rf95.send((uint8_t *)radiopacket, 20);
 
-  Serial.println("Waiting for packet to complete..."); delay(10);
-  rf95.waitPacketSent();
-  // Now wait for a reply
-  uint8_t buf[RH_RF95_MAX_MESSAGE_LEN];
-  uint8_t len = sizeof(buf);
+	Serial.println("Waiting for packet to complete..."); delay(10);
+	rf95.waitPacketSent();
+	// Now wait for a reply
+	uint8_t buf[RH_RF95_MAX_MESSAGE_LEN];
+	uint8_t len = sizeof(buf);
 
-  Serial.println("Waiting for reply..."); delay(10);
-  if (rf95.waitAvailableTimeout(1000))
-  { 
-    // Should be a reply message for us now   
-    if (rf95.recv(buf, &len))
-   {  
-      digitalWrite(LED_STBY, LOW);
-      digitalWrite(LED_RCVD, HIGH); 
-      Serial.print("Got reply: ");
-      Serial.println((char*)buf);
-      Serial.print("RSSI: ");
-      Serial.println(rf95.lastRssi(), DEC);
-      delay(250);
-      digitalWrite(LED_RCVD, LOW);    
-    }
-    else
-    {
-      Serial.println("Receive failed");
-    }
-  }
-  else
-  {
-    Serial.println("No reply, is there a listener around?");
-    digitalWrite(LED_STBY, HIGH); 
-  }
-  delay(1000);
+	Serial.println("Waiting for reply..."); delay(10);
+	if(rf95.waitAvailableTimeout(1000)){
+		// Should be a reply message for us now   
+		if(rf95.recv(buf, &len)){
+			digitalWrite(LED_STBY, LOW);
+			digitalWrite(LED_SENT, HIGH); 
+			Serial.print("Got reply: ");
+			Serial.println((char*)buf);
+			Serial.print("RSSI: ");
+			Serial.println(rf95.lastRssi(), DEC);
+			rcvd_pkt++; delay(500);
+      digitalWrite(LED_SENT, LOW);
+		} else{
+			Serial.println("Receive failed");
+			digitalWrite(LED_STBY, LOW);      
+			digitalWrite(LED_LOST, HIGH);
+			lost_pkt++;
+			delay(1000);
+		}
+	} else{
+		digitalWrite(LED_LOST, HIGH);
+		digitalWrite(LED_STBY, LOW);
+		Serial.println("No reply, is there a listener around?");
+		lost_pkt++;
+    delay(1000);
+	}
+	sent_pkt++;
+	digitalWrite(LED_SENT, LOW);
+	digitalWrite(LED_LOST, LOW); 
+  digitalWrite(LED_STBY, HIGH); delay(4000);
 }
